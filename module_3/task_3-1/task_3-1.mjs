7e72dfaae086d3c9af054cb5e2efeda2cb60d04e
SveiniXD <svennahd2017@outlook.com>
2024-10-16 14:45:59 +0200

    Update task_3-1.mjs

--- module_3/task_3-1/task_3-1.mjs
+++ module_3/task_3-1/task_3-1.mjs
@@ -4,10 +4,25 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+/*
+Complete the given "if" in the task_3-3.mjs file at part 1, statement so that it matches this: If I wake up at
+exactly 7 o'clock then I can catch the bus to school. Run the program with different values of wake-up time
+(6, 7, 8).
+*/
+
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
+let wakeUpTime = 9;
+let catchBus
+const busTime = 8;
+
+if (wakeUpTime >= busTime){
+  printOut ("You woke up at " + wakeUpTime + " o'clock. You can't catch the bus!")
+}
+if (wakeUpTime < busTime){
+  printOut ("You woke up at " + wakeUpTime + " o'clock You can catch the bus!")
+}
+if (wakeUpTime > busTime){
+  printOut ("You can take the train to school, or you have to use your car.")  
 }
 printOut(newLine);
 
